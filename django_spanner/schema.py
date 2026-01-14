4340f8b82b6c6a7056c241e81ee4e514d7fc4a4e
Sakthivel Subramanian <sakthipvmj@gmail.com>
2026-01-14 23:18:33 +0530

    Fix: Unconditionally define sql_create_fk in schema.py

--- django_spanner/schema.py
+++ django_spanner/schema.py
@@ -22,13 +22,10 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
         "CREATE TABLE %(table)s (%(definition)s) PRIMARY KEY(%(primary_key)s)"
     )
     sql_delete_table = "DROP TABLE %(table)s"
-    if os.environ.get("RUNNING_SPANNER_BACKEND_TESTS") == "1":
-        sql_create_fk = None
-    else:
-        sql_create_fk = (
-            "ALTER TABLE %(table)s ADD CONSTRAINT %(name)s FOREIGN KEY (%(column)s) "
-            "REFERENCES %(to_table)s (%(to_column)s)"
-        )
+    sql_create_fk = (
+        "ALTER TABLE %(table)s ADD CONSTRAINT %(name)s FOREIGN KEY (%(column)s) "
+        "REFERENCES %(to_table)s (%(to_column)s)"
+    )
     # Spanner doesn't support partial indexes. This string omits the
     # %(condition)s placeholder so that partial indexes are ignored.
     sql_create_index = (
